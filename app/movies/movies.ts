077df115080231762468f113076aeb59f05c8d03
arthur andrade <arthurandrad.23@gmail.com>
2024-10-13 15:00:55 -0300

    Test: Testing repository in local and cloud

--- app/movies/movies.ts
+++ app/movies/movies.ts
@@ -4,6 +4,12 @@ const movies = [
         autor: "autor",
         genero: "genero",
         imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
+    },
+    {
+        name: "nome do filme 2 ",
+        autor: "autor tal",
+        genero: "genero categoria",
+        imagem: "link do filme do site moviesposter"
     }
 ];
 
