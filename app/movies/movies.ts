d74e98056b7de1816c2a35a70b3f08847e5c4822
Joao-Scheffel-Koller <joaoscheffelkoller@gmail.com>
2024-10-13 17:02:56 -0300

    array-filmes-joao#1

--- app/movies/movies.ts
+++ app/movies/movies.ts
@@ -3,76 +3,174 @@ const movies = [
         name: "Friday the 13th",
         autor: "Sean S. Cunningham",
         genero: "Slasher",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/1/18/Friday_the_13th_The_Final_Chapter_poster.jpg",
         diretores: [
             "Robert Downey Jr.",
             "Chris Evans",
             "Scarlett Johansson"
           ],
-    }
-];
+    },
 
-const movies = [
     {
         name: "Halloween",
-        autor: "John Carpenter",
+        diretores:[
+            "John Carpenter"
+        ],
         genero: "Slasher",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
-    }
-];
+        imagem: "https://www.movieposters.com/cdn/shop/files/halloween.24x36_480x.progressive.jpg?v=1708451691"
+    },
 
-const movies = [
     {
         name: "A Nigthmare on Elm Street",
-        autor: "Wes Craven",
+        diretores:[
+            "Wes Craven"
+        ],
         genero: "Slasher",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
-    }
-];
+        imagem: "https://www.movieposters.com/cdn/shop/files/NightmareElmStreet.24X36_17846e2f-8ff2-43e6-8264-72709d689415_480x.progressive.jpg?v=1708706013"
+    },
 
-const movies = [
     {
         name: "Alien",
-        autor: "Dan O'Bannon",
+        diretores:[
+            "Dan O'Bannon",
+            "Ronald Shusett",
+            "Ridley Scott"
+        ],
         genero: "Sci-Fi",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
-    }
-];
+        imagem: "https://www.movieposters.com/cdn/shop/files/alien_romulus_480x.progressive.jpg?v=1712854636"
+    },
 
-const movies = [
     {
         name: "The Thing",
-        autor: "John W. Campbell",
+        diretores:[
+            "John W. Campbell",
+            "Bill Lancaster"
+        ],
         genero: "Sci-Fi",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
-    }
-];
+        imagem: "https://www.movieposters.com/cdn/shop/files/THING_5d1279a2-2e38-4ca0-8cb5-91f9b30b6ebb_480x.progressive.jpg?v=1693491676"
+    },
 
-const movies = [
     {
         name: "The Texas Chain Saw Massacre",
-        autor: "Kim Henkel",
+        diretores:[
+            "Kim Henkel",
+            "Tobe Hooper"
+        ],
         genero: "Horror",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
-    }
-];
+        imagem: "https://www.movieposters.com/cdn/shop/files/ItemN241523_jpg_480x.progressive.jpg?v=1721748568"
+    },
 
-
-
-const movies = [
     {
-        name: "Alien",
-        autor: "Dan O'Bannon",
-        genero: "Sci-Fi",
-        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
+        name: "Scream",
+        diretores:[
+            "Kevin Williamson",
+            "James Vanderblit"
+        ],
+        genero: "Slasher",
+        imagem: "https://www.movieposters.com/cdn/shop/files/scream.mpw.123570_480x.progressive.jpg?v=1709821180"
         
     },
+    
     {
-        name: "nome do filme 2 ",
-        autor: "autor tal",
-        genero: "genero categoria",
-        imagem: "link do filme do site moviesposter"
+        name: "The Ring",
+        diretores:[
+            "Gore Verbinski"
+        ],
+        genero: "Psychological Horror",
+        imagem: "https://upload.wikimedia.org/wikipedia/pt/1/1c/The_Ring_%282002%29.jpg"
+        
+    },
+    
+    {
+        name: "Hellraiser",
+        diretores:[
+            "Clive Barker"
+        ],
+        genero: "Supernatural",
+        imagem: "https://www.movieposters.com/cdn/shop/files/hellraiser.mpw.125062_480x.progressive.jpg?v=1708452336"
+        
+    },
+    
+    {
+        name: "The Shining",
+        diretores:[
+            "Stanley Kubrick",
+            "Diane Johnson",
+            "Stephen King"
+        ],
+        genero: "Psychological Horror",
+        imagem: "https://www.movieposters.com/cdn/shop/products/SHINING_SH103_1980_480x.progressive.jpg?v=1603988297"
+        
+    },
+
+    {
+        name: "Night of the Living Dead",
+        diretores:[
+            "George A. Romero",
+            "John Russo",
+            "Russell Streiner",
+            "Karl Hardman"
+        ],
+        genero: "Horror",
+        imagem: "https://www.movieposters.com/cdn/shop/products/4c82629d80bd543f1da8e8e0c77d8067_331c2d78-b249-4d45-92e9-55b1e6be6213_480x.progressive.jpg?v=1573588779"
+        
+    },
+
+    {
+        name: "The Exorcist",
+        diretores:[
+            "William Friedkin",
+            "William Peter Blatty",
+        ],
+        genero: "Supernatural",
+        imagem: "https://www.movieposters.com/cdn/shop/products/51eadd844c8c362c58e30e3545936d1c_08f0d111-9fb3-4cc0-bca5-65446f37ea17_480x.progressive.jpg?v=1573616005"
+        
+    },
+
+    {
+        name: "Child's Play",
+        diretores:[
+            "Don Mancini",
+            "John Lafia"
+        ],
+        genero: "Slasher",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/1/18/Friday_the_13th_The_Final_Chapter_poster.jpg"
+        
+    },
+
+    {
+        name: "The Night Flier",
+        diretores:[
+            "Mark Pavia",
+            "Stephen King"
+        ],
+        genero: "Horror",
+        imagem: "https://upload.wikimedia.org/wikipedia/en/e/ed/Cover_of_The_Night_Flier.jpg"
+        
+    },
+
+    {
+        name: "Frankenstein",
+        diretores:[
+            "James Whale",
+            "Carl Laemmle Jr",
+            "Peggy Webling"
+        ],
+        genero: "Sci-Fi",
+        imagem: "https://m.media-amazon.com/images/I/611h25Qnv5L._AC_UF894,1000_QL80_.jpg"
+        
+    },
+
+    {
+        name: "Saw",
+        diretores:[
+            "Leigh Whannell",
+            "James Wan"
+        ],
+        genero: "Slasher",
+        imagem: "https://i.pinimg.com/originals/84/04/f2/8404f2fe6a0d220189ffadae35bfbf40.jpg"  
     }
+
 ];
 
 export default movies;
