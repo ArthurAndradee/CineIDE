9eb973f1ad0cf0b35fa478c54766d8185465a68a
arthur andrade <arthurandrad.23@gmail.com>
2024-10-13 14:55:09 -0300

    Feat: Added Folder Structure

---
+++ app/movies/movies.ts
@@ -0,0 +1,10 @@
+const movies = [
+    {
+        name: "nome do filme",
+        autor: "autor",
+        genero: "genero",
+        imagem: "https://www.movieposters.com/cdn/shop/products/matrix_5c9b9eb0-6122-4787-85e7-fa81c37919af_480x.progressive.jpg?v=1639076224"
+    }
+];
+
+export default movies;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./test.css";
 
 export default function Home() {
   return (
